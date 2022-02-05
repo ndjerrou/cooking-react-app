cfa28da104cbb8a3b93d9967b039e4b140c963ca
ndjerrou <n.djerrou@gmail.com>
2022-02-05 11:22:44 +0100

    promise.all demo

---
+++ playground.js
@@ -0,0 +1,39 @@
+function makePromise1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: "ma data1"
+      });
+    }, 1000);
+  });
+}
+
+function makePromise2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: "ma data2"
+      });
+    }, 2000);
+  });
+}
+
+function makePromise3() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: "ma data3"
+      });
+    }, 3000);
+  });
+
+  //return axios.get(url): axios returns a promise, we return it to the consumer of this fn
+}
+
+const promise1 = makePromise1();
+const promise2 = makePromise2();
+const promise3 = makePromise3();
+
+Promise.all([promise1, promise2, promise3]).then((result) =>
+  console.log(result)
+);
